926153aba82aa253dd2afd2f03d922f690c9d732
Marcelo S <marcelo15011@gmail.com>
2022-06-28 19:50:22 -0300

    Projeto desenvolvido durando o Ignite Lab da Rocketseat. Desenvolvendo uma aplicação para a apresentação de videos de um evento

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
+
+function App() {
+
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
